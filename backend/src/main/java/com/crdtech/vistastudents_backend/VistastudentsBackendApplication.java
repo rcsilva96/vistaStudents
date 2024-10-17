4ae72082071c3f827d4e0d0c5d78ca5fe0213745
Renato Carvalho Silva <rsilv1822@gmail.com>
2024-10-17 05:47:19 -0300

    first commit

---
+++ backend/src/main/java/com/crdtech/vistastudents_backend/VistastudentsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crdtech.vistastudents_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VistastudentsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VistastudentsBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/crdtech/vistastudents_backend/VistastudentsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crdtech.vistastudents_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VistastudentsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
